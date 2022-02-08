8d8871acd356a1c35bd47d4a407c986e55b9181a
Kevin Wang <kevqwang@gmail.com>
2022-02-08 14:53:02 -0500

    done

--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm({addToy}) {
+
+const [ name, setName ] = useState('')
+const [ image, setImage ] = useState('')
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const formData = {
+   name: name,
+   image: image 
+  }
+  fetch('http://localhost:3001/toys' , {
+    method: "POST",
+    headers: {
+      "Content-Type" : 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(res => res.json())
+  .then(data  => addToy(data))
+}
+
+
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          onChange={e => setName(e.target.value)}
         />
         <br />
         <input
@@ -17,6 +41,7 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          onChange={e => setImage(e.target.value)}
         />
         <br />
         <input
